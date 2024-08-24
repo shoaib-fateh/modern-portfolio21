01a106d9d7b2c376ae63dbc4a86f78a275ef1a8a
Shoaib Fateh <shoaibfateh21@yahoo.com>
2024-08-24 21:35:52 +0430

    added theme provider

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -10,6 +10,16 @@ function Hero() {
       />
       <Spotlight className="top-10 left-full h-[80vh] w-[50vw]" fill="purple" />
       <Spotlight className="top-28 left-80 h-[80vh] w-[50wh]" fill="blue" />
+
+      <div
+        className="h-screen w-full dark:bg-black-100 bg-white dark:bg-grid-white/[0.03] bg-grid-black-100/[0.2]
+       absolute top-0 left-0 flex items-center justify-center"
+      >
+        <div
+          className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black-100
+         bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"
+        />
+      </div>
     </div>
   );
 }
