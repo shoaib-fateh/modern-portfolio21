82c846ee800f4ecbbb616f490c99099c7a14ebf5
Shoaib Fateh <shoaibfateh21@yahoo.com>
2024-08-24 20:59:51 +0430

    added Hero component

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,13 @@
 "use client";
 
+import Hero from "@/components/Hero";
+
 const Page = () => {
   return (
     <main className="relative bg-black-100 flex justify-center items-center flex-col mx-auto sm:px-10 px-5">
-      <div className="max-w-7xl w-full"></div>
-      <h1>My Portfolio</h1>
+      <div className="max-w-7xl w-full">
+        <Hero />
+      </div>
     </main>
   );
 };
