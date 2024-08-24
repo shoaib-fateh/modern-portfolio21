4a7bd2bf020565450d2c965e673773bd549f371e
Shoaib Fateh <shoaibfateh21@yahoo.com>
2024-08-24 21:50:52 +0430

    added text generate effects / fixed some typos

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Spotlight } from "./ui/Spotlight";
+import { TextGenerateEffect } from "./ui/TextGenerateEffect";
 
 function Hero() {
   return (
@@ -26,6 +27,11 @@ function Hero() {
           <h2 className="uppercase tracking-widest text-xs text-center text-blue-100 max-w-80">
             Dynamic Web Magic with Next.js
           </h2>
+
+          <TextGenerateEffect
+            className="text-center text-[40px] md:text-5xl lg:text-6xl"
+            words="Transforming Concepts into seamless Experiences"
+          />
         </div>
       </div>
     </div>
