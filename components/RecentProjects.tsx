76f3065a7bca63c14c614a3ca058c9b90afbbeb7
Shoaib Fateh <shoaibfateh21@yahoo.com>
2024-08-26 22:34:26 +0430

    added recent project component / pin container

--- components/RecentProjects.tsx
+++ components/RecentProjects.tsx
@@ -1,7 +1,66 @@
+import { projects } from "@/data";
 import React from "react";
+import { PinContainer } from "./ui/PinContainer";
+import { FaLocationArrow } from "react-icons/fa6";
 
 function RecentProjects() {
-  return <div>RecentProjects</div>;
+  return (
+    <div className="py-20">
+      <h1 className="font-bold text-4xl md:text-5xl text-center">
+        A Small Selection of <div className="text-purple">recent projects</div>
+      </h1>
+
+      <div className="flex flex-wrap items-center justify-center p-4 gap-16 mt-10">
+        {projects.map(({ id, title, link, img, des, iconLists }) => (
+          <div
+            key={id}
+            className="sm:h-[41rem] h-[32rem] sm:w-[570px]  lg:min-h-[32.5rem] flex items-center justify-center w-[80vw]"
+          >
+            <PinContainer title={link} href={link}>
+              <div className="relative flex items-center justify-center sm:w-[570px] sm:h-[40vh] h-[30vh] w-[80vw] overflow-hidden mb-10">
+                <div className="relative w-full h-full overflow-hidden lg:rounded-3xl bg-[#13162d] ">
+                  <img src="./bg.png" alt="bg-img" />
+                </div>
+                <img
+                  src={img}
+                  alt={title}
+                  className="z-10 absolute bottom-10"
+                />
+              </div>
+              <h1 className="font-bold lg:text-2xl md:text-xl text-base line-clamp-1">
+                {title}
+              </h1>
+              <p className="lg:text-xl lg:font-normal font-light text-sm line-clamp-2">
+                {des}
+              </p>
+
+              <div className="flex items-center justify-between mt-7 mb-3">
+                <div className="flex items-center">
+                  {iconLists.map((icon, index) => (
+                    <div
+                      key={icon}
+                      className="border border-white/[0.2] bg-black rounded-full lg:w-10 lg:h-10 w-8 h-8 flex justify-center items-center"
+                      style={{
+                        transform: `translateX(-${5 * index * 2}px)`,
+                      }}
+                    >
+                      <img src={icon} alt={icon} className="p-2" />
+                    </div>
+                  ))}
+                </div>
+                <div className="flex justify-center items-center">
+                  <p className="flex lg:text-xl md:text-xs test-sm text-purple">
+                    Check Live Site
+                  </p>
+                  <FaLocationArrow className="ms-3" color="#CBACE9" />
+                </div>
+              </div>
+            </PinContainer>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default RecentProjects;
