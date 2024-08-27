f7558d30bdf7b5772d68c6ec4e1bb5c38824d6a9
Shoaib Fateh <shoaibfateh21@yahoo.com>
2024-08-28 00:15:30 +0430

    updated

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,18 +1,23 @@
-import React from "react";
+import { FaLocationArrow } from "react-icons/fa6";
+
 import { Spotlight } from "./ui/Spotlight";
 import { TextGenerateEffect } from "./ui/TextGenerateEffect";
 import MagicButton from "./ui/MagicButton";
-import { FaLocationArrow } from "react-icons/fa6";
 
-function Hero() {
+const Hero = () => {
   return (
     <div className="pb-20 pt-36">
-      <Spotlight
-        className="-top-40 -left-10 md:-left-32 md:-top-20 h-screen"
-        fill="white"
-      />
-      <Spotlight className="top-10 left-full h-[80vh] w-[50vw]" fill="purple" />
-      <Spotlight className="top-28 left-80 h-[80vh] w-[50wh]" fill="blue" />
+      <div>
+        <Spotlight
+          className="-top-40 -left-10 md:-left-32 md:-top-20 h-screen"
+          fill="white"
+        />
+        <Spotlight
+          className="h-[80vh] w-[50vw] top-10 left-full"
+          fill="purple"
+        />
+        <Spotlight className="left-80 top-28 h-[80vh] w-[50vw]" fill="blue" />
+      </div>
 
       <div
         className="h-screen w-full dark:bg-black-100 bg-white dark:bg-grid-white/[0.03] bg-grid-black-100/[0.2]
@@ -25,24 +30,23 @@ function Hero() {
       </div>
 
       <div className="flex justify-center relative my-20 z-10">
-        <div className="max-w-[89wv] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center">
-          <h2 className="uppercase tracking-widest text-xs text-center text-blue-100 max-w-80">
+        <div className="max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center">
+          <p className="uppercase tracking-widest text-xs text-center text-blue-100 max-w-80">
             Dynamic Web Magic with Next.js
-          </h2>
+          </p>
 
           <TextGenerateEffect
+            words="Transforming Concepts into Seamless User Experiences"
             className="text-center text-[40px] md:text-5xl lg:text-6xl"
-            words="Transforming Concepts into seamless User Experiences"
           />
 
-          <p>
-            Hi, I&apos;m Fateh, a Next.js Developer based in Developer&apos;s
-            World!
+          <p className="text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl">
+            Hi! I&apos;m Adrian, a Next.js Developer based in Croatia.
           </p>
 
           <a href="#about">
             <MagicButton
-              title="Show My Works"
+              title="Show my work"
               icon={<FaLocationArrow />}
               position="right"
             />
@@ -51,6 +55,6 @@ function Hero() {
       </div>
     </div>
   );
-}
+};
 
 export default Hero;
