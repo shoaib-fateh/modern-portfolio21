5ae04f4cb1371e25e1d7bbcf4bce0437d0749f4d
Shoaib Fateh <shoaibfateh21@yahoo.com>
2024-08-29 13:23:40 +0430

    updated

--- components/RecentProjects.tsx
+++ components/RecentProjects.tsx
@@ -1,36 +1,42 @@
-import { projects } from "@/data";
-import React from "react";
-import { PinContainer } from "./ui/PinContainer";
+"use client";
+
 import { FaLocationArrow } from "react-icons/fa6";
 
-function RecentProjects() {
-  return (
-    <section id="#projects" className="py-20">
-      <h1 className="font-bold text-4xl md:text-5xl text-center">
-        A Small Selection of <div className="text-purple">Recent Projects</div>
-      </h1>
+import { projects } from "@/data";
+import { PinContainer } from "./ui/Pin";
 
+const RecentProjects = () => {
+  return (
+    <div className="py-20">
+      <h1 className="heading">
+        A small selection of{" "}
+        <span className="text-purple">recent projects</span>
+      </h1>
       <div className="flex flex-wrap items-center justify-center p-4 gap-16 mt-10">
         {projects.map(({ id, title, link, img, des, iconLists }) => (
           <div
             key={id}
-            className="sm:h-[41rem] h-[32rem] sm:w-[570px]  lg:min-h-[32.5rem] flex items-center justify-center w-[80vw]"
+            className="flex items-center justify-center w-full sm:w-96 lg:w-[45%] h-[25rem] lg:min-h-[32.5rem] mb-10"
           >
             <PinContainer title={link} href={link}>
               <div className="relative flex items-center justify-center sm:w-[570px] sm:h-[40vh] h-[30vh] w-[80vw] overflow-hidden mb-10">
                 <div className="relative w-full h-full overflow-hidden lg:rounded-3xl bg-[#13162d] ">
                   <img src="./bg.png" alt="bg-img" />
                 </div>
-                <img
-                  src={img}
-                  alt={title}
-                  className="z-10 absolute bottom-10"
-                />
+                <img src={img} alt="cover" className="z-10 absolute bottom-0" />
               </div>
+
               <h1 className="font-bold lg:text-2xl md:text-xl text-base line-clamp-1">
                 {title}
               </h1>
-              <p className="lg:text-xl lg:font-normal font-light text-sm line-clamp-2">
+
+              <p
+                className="lg:text-xl lg:font-normal font-light text-sm line-clamp-2"
+                style={{
+                  color: "#BEC1DD",
+                  margin: "1vh 0",
+                }}
+              >
                 {des}
               </p>
 
@@ -38,29 +44,30 @@ function RecentProjects() {
                 <div className="flex items-center">
                   {iconLists.map((icon, index) => (
                     <div
-                      key={icon}
-                      className="border border-white/[0.2] bg-black rounded-full lg:w-10 lg:h-10 w-8 h-8 flex justify-center items-center"
+                      key={index}
+                      className="border border-white/[.2] rounded-full bg-black lg:w-10 lg:h-10 w-8 h-8 flex justify-center items-center"
                       style={{
-                        transform: `translateX(-${5 * index * 2}px)`,
+                        transform: `translateX(-${5 * index + 2}px)`,
                       }}
                     >
-                      <img src={icon} alt={icon} className="p-2" />
+                      <img src={icon} alt="icon5" className="p-2" />
                     </div>
                   ))}
                 </div>
+
                 <div className="flex justify-center items-center">
-                  <p className="flex lg:text-xl md:text-xs test-sm text-purple">
+                  <p className="flex lg:text-xl md:text-xs text-sm text-purple">
                     Check Live Site
                   </p>
-                  <FaLocationArrow className="ms-3" color="#CBACE9" />
+                  <FaLocationArrow className="ms-3" color="#CBACF9" />
                 </div>
               </div>
             </PinContainer>
           </div>
         ))}
       </div>
-    </section>
+    </div>
   );
-}
+};
 
 export default RecentProjects;
--- components/ui/Pin.tsx
+++ components/ui/Pin.tsx
@@ -1,8 +1,8 @@
 "use client";
 import React, { useState } from "react";
 import { motion } from "framer-motion";
+
 import { cn } from "@/lib/utils";
-import Link from "next/link";
 
 export const PinContainer = ({
   children,
@@ -29,14 +29,13 @@ export const PinContainer = ({
   };
 
   return (
-    <Link
+    <div
       className={cn(
         "relative group/pin z-50  cursor-pointer",
         containerClassName
       )}
       onMouseEnter={onMouseEnter}
       onMouseLeave={onMouseLeave}
-      href={href || "/"}
     >
       <div
         style={{
@@ -49,13 +48,14 @@ export const PinContainer = ({
           style={{
             transform: transform,
           }}
+          // remove  bg-black
           className="absolute left-1/2 p-4 top-1/2  flex justify-start items-start  rounded-2xl  shadow-[0_8px_16px_rgb(0_0_0/0.4)] border border-white/[0.1] group-hover/pin:border-white/[0.2] transition duration-700 overflow-hidden"
         >
           <div className={cn(" relative z-50 ", className)}>{children}</div>
         </div>
       </div>
       <PinPerspective title={title} href={href} />
-    </Link>
+    </div>
   );
 };
 
@@ -67,7 +67,8 @@ export const PinPerspective = ({
   href?: string;
 }) => {
   return (
-    <motion.div className="pointer-events-none  w-96 h-80 flex items-center justify-center opacity-0 group-hover/pin:opacity-100 z-[60] transition duration-500">
+    // change w-96 to w-full
+    <motion.div className="pointer-events-none w-full h-80 flex items-center justify-center opacity-0 group-hover/pin:opacity-100 z-[60] transition duration-500">
       <div className=" w-full h-full -mt-7 flex-none  inset-0">
         <div className="absolute top-0 inset-x-0  flex justify-center">
           <a
@@ -75,13 +76,11 @@ export const PinPerspective = ({
             target={"_blank"}
             className="relative flex space-x-2 items-center z-10 rounded-full bg-zinc-950 py-0.5 px-4 ring-1 ring-white/10 "
           >
-            <div>
-              <span className="relative z-20 text-white text-xs font-bold inline-block py-0.5">
-                {title}
-              </span>
+            <span className="relative z-20 text-white text-xs font-bold inline-block py-0.5">
+              {title}
+            </span>
 
-              <span className="absolute -bottom-0 left-[1.125rem] h-px w-[calc(100%-2.25rem)] bg-gradient-to-r from-emerald-400/0 via-emerald-400/90 to-emerald-400/0 transition-opacity duration-500 group-hover/btn:opacity-40"></span>
-            </div>
+            <span className="absolute -bottom-0 left-[1.125rem] h-px w-[calc(100%-2.25rem)] bg-gradient-to-r from-emerald-400/0 via-emerald-400/90 to-emerald-400/0 transition-opacity duration-500 group-hover/btn:opacity-40"></span>
           </a>
         </div>
 
